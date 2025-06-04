b5784c37dbdd578bf5afb55e1f583810bf068b8a
Marcelo Ferreira <marcelo.conceicao1718@gmail.com>
2025-06-04 19:47:11 -0300

    Update TelaCadastroPratos.jsx

--- src/pages/TelaCadastroPratos/TelaCadastroPratos.jsx
+++ src/pages/TelaCadastroPratos/TelaCadastroPratos.jsx
@@ -32,7 +32,7 @@ const CadastrarPrato = () => {
     try {
       console.log('Enviando dados para o backend:', formData);
 
-      const response = await axios.post('http://localhost:8080/Prato', formData);
+      const response = await axios.post('https://atividadedeploy-1b4f.onrender.com', formData);
 
       alert('Prato cadastrado com sucesso!');
       navigate('/');
