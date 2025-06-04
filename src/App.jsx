b37012839a5dd06dcb1169fd4040d531ad5b3982
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-06-04 20:51:45 -0300

    Agora vai

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import HomePage from "./pages/TelaInicial/homepage";
+import HomePage from "./pages/TelaInicial/TelaInicial";
 import CardapioPage from "./pages/TelaCardápio/CardapioPage";
 import CadastroPratosPage from "./pages/TelaCadastroPratos/TelaCadastroPratos";
 
