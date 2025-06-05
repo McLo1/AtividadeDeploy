063cf97321bd258325aef5085d2ad29fe6d680c4
Marcelo Ferreira <marcelo.conceicao1718@gmail.com>
2025-06-04 21:03:34 -0300

    Update PratoController.java

--- src/main/java/Backend/Backend/controller/PratoController.java
+++ src/main/java/Backend/Backend/controller/PratoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 @RestController
-@CrossOrigin (origins = "*")
+@CrossOrigin (origins = "https://atividadedeploy-1.onrender.com")
 @RequestMapping("/Prato")
 public class PratoController {
 
